518c700b0dc887bc96bc134a6fc46434b09c271d
mahdi hosseini <hosseinim018@gmail.com>
2023-08-17 17:12:51 +0330

    craete __init__.py module and import functions from Text.py and initialize __version__ and __all__

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from .Text import Spoiler, Mono, Bold, Italic, Hashtag, Underline, CreateLink, Strikethrough, Mention, inlineMention, INIsection
+
+__version__ = '0.1.0'
+
+__all__ = [
+    "Spoiler",
+    "Mono",
+    "Bold",
+    "Italic",
+    "Hashtag",
+    "Underline",
+    "CreateLink",
+    "Strikethrough",
+    "Mention",
+    "inlineMention",
+    "INIsection",
+]
