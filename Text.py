bf0c5eab92617b2dae19fff064c16122d32593ba
mahdi hosseini <hosseinim018@gmail.com>
2023-08-17 17:06:09 +0330

    craete add_tags function

---
+++ Text.py
@@ -0,0 +1,18 @@
+from typing import Union, List
+
+def add_tags(tag: str, text: str) -> str:
+    """
+    Adds a specified tag at the start and end of the given text.
+
+    Parameters:
+    tag (str): The tag to be added. It should be a string.
+    text (str): The text to be tagged. It should be a string.
+
+    Returns:
+    str: The tagged text, with the specified tag added at the start and end.
+
+    Example:
+    >>> add_tags('<i>', 'Python')
+    '<i>Python<i>'
+    """
+    return f"<{tag}>{text}</{tag}>"
